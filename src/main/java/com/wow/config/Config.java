eeaa53bbe0119b6c6f4d0ff2b79148c00c98dd90
Izonami <kuokatsu@gmail.com>
2015-10-13 11:23:37 +0600

    Add config properties

---
+++ src/main/java/com/wow/config/Config.java
@@ -0,0 +1,16 @@
+package com.wow.config;
+
+/**
+ * Created by kuksin-mv on 13.10.2015.
+ */
+public class Config extends ConfigEngine
+{
+    public static final String CHARACTER_CONFIG_FILE = "./config/Character.properties";
+    public static final String LOGIN_CONFIGURATION_FILE = "./config/LoginServer.properties";
+    public static final String CONFIGURATION_FILE = "./config/Server.properties";
+
+    public static void load()
+    {
+
+    }
+}
