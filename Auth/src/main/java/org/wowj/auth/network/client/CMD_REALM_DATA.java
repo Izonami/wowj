03f084aac0bd74d8e676dd38190744be125b7671
Izonami <kuokatsu@gmail.com>
2015-10-15 15:11:51 +0600

    Start work with packet

---
+++ Auth/src/main/java/org/wowj/auth/network/client/CMD_REALM_DATA.java
@@ -0,0 +1,10 @@
+package org.wowj.auth.network.client;
+
+import org.wowj.auth.network.AbstractWoWClientPacket;
+
+/**
+ * Created by kuksin-mv on 15.10.2015.
+ */
+public class CMD_REALM_DATA extends AbstractWoWClientPacket
+{
+}
---
+++ Auth/src/main/java/org/wowj/auth/network/server/SMD_AUTH_ENABLE_CRYPT.java
@@ -0,0 +1,10 @@
+package org.wowj.auth.network.server;
+
+import org.wowj.auth.network.AbstractWoWServerPacket;
+
+/**
+ * Created by kuksin-mv on 15.10.2015.
+ */
+public class SMD_AUTH_ENABLE_CRYPT extends AbstractWoWServerPacket
+{
+}
